a53d02e93a4c8a209b52e4fdb1d0e5ed8f7ad362
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-07-27 22:48:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+
+	// If str is null or size is not valid, return empty array
+  if (!str || size <= 0) return [];
+
+  const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    // Slice from current index up to size
+    result.push(str.slice(i, i + size));
+  }
+  return result;
 }
 
 // Do not change the code below
